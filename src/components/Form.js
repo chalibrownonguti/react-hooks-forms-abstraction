373e317d6b670798e3639eeebf948dd8a96df7d2
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-04 11:21:54 +0300

    Form update

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,38 +2,52 @@ import React, { useState } from "react";
 
 function Form() {
   const [formData, setFormData] = useState({
-    firstName: "John",
-    lastName: "Henry",
+    firstName: "Sylvia",
+    lastName: "Woods",
+    admin: false,
   });
 
-  function handleFirstNameChange(event) {
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    // use `checked` property of checkboxes instead of `value`
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
     setFormData({
       ...formData,
-      firstName: event.target.value,
+      [name]: value,
     });
   }
 
-  function handleLastNameChange(event) {
-    setFormData({
-      ...formData,
-      lastName: event.target.value,
-    });
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
-  type="text"
-  name="firstName"
-  value={formData.firstName}
-  onChange={handleFirstNameChange}
-/>
-<input
-  type="text"
-  name="lastName"
-  value={formData.lastName}
-  onChange={handleLastNameChange}
-/>
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 }
